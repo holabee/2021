ed067db73dbc29775eb11a3c47455707b574ba99
holabee <function.hola.b@gmail.com>
2021-01-07 16:23:02 +0900

    아직 뭐할지 결정 못함..

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          <img src={logo} className="App-logo" alt="logo" />
+        </h1>
       </header>
+      <nav>
+        
+      </nav>
+      <main></main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
